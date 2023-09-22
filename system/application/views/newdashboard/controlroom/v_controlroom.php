c588ecafd94b1997cee755561c88a95b527c723b
DESKTOP-24P520E\Win 10 <iqbalalyansyah3@gmail.com>
2023-09-22 10:18:12 +0700

    update chart

--- system/application/views/newdashboard/controlroom/v_controlroom.php
+++ system/application/views/newdashboard/controlroom/v_controlroom.php
@@ -1,59 +1,9 @@
-<script src="<?php echo base_url(); ?>assets/dashboard/assets/plugins/jquery/jquery.min.js"></script>
+<script src="https://code.highcharts.com/highcharts.js"></script>
 
-<!-- <link rel="stylesheet" href="<?php base_url(); ?>assets/dashboard/assets/plugins/bootstrap-table-1.19.1/extensions/sticky-header/bootstrap-table-sticky-header.css">
+
 <link href="<?php base_url(); ?>assets/dashboard/assets/plugins/bootstrap-table-1.19.1/bootstrap-table.min.css" rel="stylesheet">
 <script src="<?php base_url(); ?>assets/dashboard/assets/plugins/bootstrap-table-1.19.1/extensions/sticky-header/bootstrap-table-sticky-header.js"></script>
-<script src="<?php base_url(); ?>assets/dashboard/assets/plugins/bootstrap-table-1.19.1/bootstrap-table.min.js"></script> -->
-
-<!-- Include Chart.js library -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-<style type="text/css">
-    /* edit style datepicker*/
-    .datetimepicker {
-        background: #1F50A2;
-    }
-
-    .prev,
-    .switch,
-    .next,
-    .today {
-        background: #FFF;
-    }
-
-    .dow {
-        color: #FFF;
-        padding: 6px;
-    }
-
-    .table-condensed tbody tr td {
-        color: #FFF;
-    }
-
-    .datetimepicker .datetimepicker-days table tbody tr td:hover {
-        background-color: #000;
-    }
-
-    .datetimepicker .datetimepicker-years table tbody tr td span:hover {
-        background-color: #000;
-    }
-
-    .datetimepicker .datetimepicker-months table tbody tr td span:hover {
-        background-color: #000;
-    }
-
-
-
-    /* edit Style graphic */
-    .highcharts-data-label text {
-        text-decoration: none;
-    }
-
-    .highcharts-drilldown-axis-label {
-        pointer-events: none;
-    }
-</style>
+<script src="<?php base_url(); ?>assets/dashboard/assets/plugins/bootstrap-table-1.19.1/bootstrap-table.min.js"></script> 
 
 <script>
     function frmsearch_onsubmit() {
@@ -88,25 +38,35 @@
         //     p = 0;
         // }
         // jQuery("#offset").val(p);
-        jQuery("#lineChart").hide();
+        jQuery("#result").hide();
         jQuery("#loader").show();
 
         jQuery.post("<?= base_url(); ?>hse/search_violation2", jQuery("#frmsearch").serialize(),
-            function(r) {
-                if (r.error) {
-                    console.log(r);
-                    alert(r.message);
-                    jQuery("#loader").hide();
-                    jQuery("#lineChart").hide();
-                    return;
-                } else {
-                    console.log(r);
-                    jQuery("#loader").hide();
-                    jQuery("#lineChart").html(r.html);
-                    jQuery("#lineChart").show();
-                }
-            }, "json"
-        );
+        function (r) {
+            if (r.error) {
+                console.log(r);
+                alert(r.message);
+                jQuery("#loader").hide();
+                jQuery("#result").hide();
+                return;
+            } else {
+                console.log(r);
+
+                // Update data grafik chart1 dan chart2 dengan data dari hasil pencarian
+                dataChart1.series[0].data = r.dataChart1True; // Ganti dengan data True dari hasil pencarian
+                dataChart1.series[1].data = r.dataChart1False; // Ganti dengan data False dari hasil pencarian
+                Highcharts.chart('chart1', dataChart1);
+
+                dataChart2.series[0].data = r.dataChart2True; // Ganti dengan data True dari hasil pencarian
+                dataChart2.series[1].data = r.dataChart2False; // Ganti dengan data False dari hasil pencarian
+                Highcharts.chart('chart2', dataChart2);
+
+                jQuery("#loader").hide();
+                jQuery("#result").html(r.html);
+                jQuery("#result").show();
+            }
+          }, "json"
+      );
     }
 
     function periode_onchange() {
@@ -122,7 +82,7 @@
 
     }
 
-    function company_onchange() {
+      function company_onchange() {
         var data_company = jQuery("#company").val();
         var dc = data_company.split("@");
         var site = "<?= base_url() ?>hse/get_vehicle_by_company/" + dc[0];
@@ -135,21 +95,28 @@
             dataType: "html"
         });
     }
-    </script>
-    <!-- start sidebar menu -->
+  </script>
+
+</script>
+
+</head>
+
+<body>
+ <!-- start sidebar menu -->
     <div class="sidebar-container">
         <?= $sidebar; ?>
     </div>
     <!-- end sidebar menu -->
+
     <!-- start page content -->
     <div class="page-content-wrapper">
-        <div class="page-content">
+         <div class="page-content">
             <form class="form-horizontal" name="frmsearch" id="frmsearch" onsubmit="javascript:return frmsearch_onsubmit()">
-                <div class="row">
-                    <div class="col-md-12 col-sm-12">
-                        <div class="panel" id="panel_form">
-                            <div class="card-header" style="text-align: center; font-size:large;">
-                                <b>DASHBOARD PROFILE CONTROL ROOM</b>
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <div class="panel"id="myChart">
+                        <div class="card-header" style="text-align: center; font-size:large;">
+                            <b>Dashboard Profile Control Room</b>
                         </div>
                         <div class="panel-body" id="bar-parent10">
                             <div class="form-group row">
@@ -168,7 +135,7 @@
                                         }
                                         ?>
                                     </select>
-                                </div>
+                                    </div>
                                 <div class="col-lg-2 col-md-2" style="display: none;">
                                     <select id="vehicle" name="vehicle" class="form-control select2">
                                         <option value="all">--All Vehicle</option>
@@ -181,16 +148,12 @@
                                     </select>
                                 </div>
                                 <div class="col-lg-2 col-md-2">
-                                    <?php // var_dump($alarmtype) 
-                                    ?>
-                                    <select id="violation" name="violation" class="form-control select2">
-                                        <option value="all">--All Violation</option>
+                                    <select id="violationmasterselect" name="violationmasterselect" class="form-control select2" onchange="onchangefilter()">
+                                        <option value="0">--All Violation</option>
                                         <?php
-                                        $cviolation = count($rviolation);
-                                        for ($i = 0; $i < $cviolation; $i++) {
-                                            echo "<option value='" . $rviolation[$i]["alarmmaster_id"] . "'>" . $rviolation[$i]["alarmmaster_name"] . "</option>";
-                                        }
-                                        ?>
+                                        for ($i = 0; $i < sizeof($violationmaster); $i++) {?>
+                                        <option value=<?php echo $violationmaster[$i]["alarmmaster_id"] ?>><?php echo $violationmaster[$i]["alarmmaster_name"] ?></option>
+                                        <?php } ?>
                                     </select>
                                 </div>
                                 <div class="col-lg-2 col-md-2">
@@ -208,7 +171,7 @@
                                     </select>
                                 </div>
                                 <div class="col-lg-2 col-md-2">
-                                    <button class="btn btn-circle btn-success" id="btnsearchreport" type="submit">Search</button>
+                                    <button class="btn btn-circle btn-success" id="btnsearchreport" type="submit" style="margin-left: 30px;">Search</button>
                                     <!-- <img id="loader2" style="display:none;" src="<?php echo base_url(); ?>assets/images/ajax-loader.gif" /> -->
                                 </div>
                             </div>
@@ -241,188 +204,81 @@
 
                 </div>
 
-
             </div>
-        </form>
+    </form>
 
-        <canvas id="lineChart"></canvas>
+    <div id="chart1" style="width: 50%; float: left;"></div>
+    <div id="chart2" style="width: 50%; float: left;"></div>
 
-        <div id="modalStatev" class="modal" style="height: 100%;">
-            <div class="modal-content-state">
-                <div class="row">
-                    <div class="col-md-10">
-                        <p class="modalTitleforAll" id="modalStateTitle">
-                            <button type="button" name="button" id="export_xcel_info" class="btn btn-primary btn-sm">Export Excel</button>
-                        </p>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="btn btn-danger btn-sm" onclick="closemodalviolation()">X</div>
-                    </div>
-                </div>
-                <div id="modalStateContent">
-                    <table class="table table-striped table-bordered" id="contenttable" style="font-size: 12px; text-align:center;">
+    <script>
 
-                    </table>
-                </div>
-                <div id="divtoUpload" style="display:none"></div>
-            </div>
-        </div>
+        
+    $("#btnsearchreport").click(function (e) {
+        e.preventDefault();
+        const company = $("#company").val();
+        const violation = $("#violation").val();
+        const periode = $("#periode").val();
+        const startDate = $("#startdate").val();
+        const endDate = $("#endtdate").val();
 
-    </div>
-    <!-- end page content -->
-    <!--<div id="loader2" class="mdl-progress mdl-js-progress mdl-progress__indeterminate" ></div>-->
-
-</div>
-<!-- end page container -->
-
-<script type="text/javascript">
-
-    
-function collect_data() {
-        var data_hour = '<?php echo json_encode($data_hour); ?>';
-        var data_hour_fix = JSON.parse(data_hour);
-        var data_company = '<?php echo json_encode($data_company); ?>';
-        var data_company_fix = JSON.parse(data_company);
-        var opposite_company = '<?php echo json_encode($opposite_company); ?>';
-        var opposite_company_fix = JSON.parse(opposite_company);
-        var data_violation = '<?php echo json_encode($data_alarm); ?>';
-        var data_violation_fix = JSON.parse(data_violation);
-        var total_data = <?= $total_data; ?>;
-        var date = '<?= $date; ?>';
-        var data = '<?php echo json_encode($data_fix); ?>';
-        var data_fix = JSON.parse(data);
-
-        var data_table = '<?php echo json_encode($data_table); ?>';
-        var data_table_fix = JSON.parse(data_table);
-        var master_company = '<?php echo json_encode($master_company); ?>';
-        var master_company_fix = JSON.parse(master_company);
-        var master_v = '<?php echo json_encode($master_vehicle); ?>';
-        var master_violation = '<?php echo json_encode($master_violation); ?>';
-        var master_violation_fix = JSON.parse(master_violation);
-        var master_v = '<?php echo json_encode($total_violation_units); ?>';
-        var total_violation_units = JSON.parse(master_v);
-        var master_v = '<?php echo json_encode($top_violation); ?>';
-        var top_violationn = JSON.parse(master_v);
-
-
-        <?php if ($input['company'] == "all") {
-            if ($input['violation'] == "all") { ?>
-                setTimeout(function() {
-                    charttrilevel(data_fix, data_hour_fix, data_company_fix, data_violation_fix, total_data, date);
-                }, 0);
-            <?php } else if ($input['violation'] == "6") { ?>
-                setTimeout(function() {
-                    charttrileveloverspeed(data_fix, data_hour_fix, data_company_fix, data_violation_fix, total_data, date);
-                }, 0);
-
-            <?php
-            } else { ?>
-                setTimeout(function() {
-                    charttwolevel(data_fix, data_hour_fix, data_company_fix, data_violation_fix, total_data, date);
-                }, 0);
-            <?php
-            }
-        } else {
-            if ($input['violation'] == "all") { ?>
-                setTimeout(function() {
-                    charttwolevel(data_fix, data_hour_fix, data_company_fix, data_violation_fix, total_data, date);
-                }, 0);
-            <?php } else { ?>
-                setTimeout(function() {
-                    chartonelevel(data_fix, data_hour_fix, data_company_fix, data_violation_fix, total_data, date);
-                }, 0);
-        <?php
-            }
-        } ?>
-        setTimeout(function() {
-            createTable(data_table_fix, data_hour_fix, data_company_fix, data_violation_fix, total_data, date, master_company_fix, master_violation_fix, opposite_company_fix, master_vehicle, total_violation_units);
-        }, 0);
-
-
-      }
-            
-    $(document).ready(function() {
-        //edit datepicker
-        $(".glyphicon-arrow-right").html(">>");
-        $(".glyphicon-arrow-left").html("<<");
-
-        // buildTable($table);
-        // $("#modalStatev").show();
-        page(0);
-
-        //export excel
-        jQuery("#export_xcel_info").click(function() {
-            var title = $("#contenttable .titletable").html();
-            var isi = $('#modalStateContent').html();
-            $("#divtoUpload").html(isi);
-            $("#divtoUpload .attachment").html("link");
-            // $("#divtoUpload .attachment2").html("link video");
-            var myBlob = new Blob([$("#divtoUpload").html()], {
-                type: 'application/vnd.ms-excel'
-            });
-            var url = window.URL.createObjectURL(myBlob);
-            var a = document.createElement("a");
-            document.body.appendChild(a);
-            a.href = url;
-            a.download = title + ".xls";
-            a.click();
-            setTimeout(function() {
-                window.URL.revokeObjectURL(url);
-            }, 0);
-
-        });
-
+        drawCharts(company, violation, periode, startDate, endDate);
     });
-</script>
 
-<script>
-     // menruskan data kedalam bentuk json
-     var chartData = <?php echo json_encode($chart_data); ?>;
+    </script>
 
-            // membuat point
-            var labels = chartData.map(item => item.alarm_report_start_time);
-            var dataPoints = chartData.map(item => item.alarm_report_statusinterventation_up);
-
-            // Get the canvas element
-            var ctx = document.getElementById('lineChart').getContext('2d');
-
-            // Buat line chart
-            var lineChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: labels,
-                    datasets: [{
-                        label: 'Alarm Report True/False Count',
-                        data: dataPoints,
-                        backgroundColor: 'rgba(0, 123, 255, 0.5)',
-                        borderColor: 'rgba(0, 123, 255, 1)',
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    scales: {
-                        xAxes: [{
-                            type: 'time',
-                            time: {
-                                unit: 'day'
-                            },
-                            scaleLabel: {
-                                display: true,
-                                labelString: 'Time'
-                            }
-                        }],
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            },
-                            scaleLabel: {
-                                display: true,
-                                labelString: 'Count'
-                            }
-                        }]
-                    }
+    <script>
+        // Data untuk grafik pertama
+        var data1 = {
+            title: {
+                text: 'DASHBOARD TRUE-FALSE ALARM',
+                subtitle: {
+                        text: 'Periode', // Tambahkan subjudul di sini
                 }
-            });
-</script>
+            },
+            xAxis: {
+                categories: ['Data A', 'Data B', 'Data C', 'Data D', 'Data E']
+            },
+            series: [{
+                name: 'True',
+                data: [10, 4, 7, 2, 8],
+                color: 'green' // Warna untuk seri "True"
+            },
+            {
+                name: 'False', // Nama series baru
+                data: [3, 6, 8, 1, 5], // Data series baru
+                color: 'black'
+            }]
+        };
+
+        // Data untuk grafik kedua
+        // Data untuk grafik kedua
+        var data2 = {
+            title: {
+                text: 'DASHBOARD LEAD TIME INTERVENSI'
+            },
+            xAxis: {
+                categories: ['Data A', 'Data B', 'Data C', 'Data D', 'Data E']
+            },
+            series: [{
+                name: 'Delay',
+                data: [10, 4, 7, 2, 8],
+                color: 'green'
+            },
+            {
+                name: 'On time', // Nama series baru
+                data: [3, 6, 8, 1, 5], // Data series baru
+                color: 'black'
+            }]
+        };
+
+        // Inisialisasi grafik kedua
+        Highcharts.chart('chart2', data2);
+
+        // Inisialisasi grafik pertama
+        Highcharts.chart('chart1', data1);
+
+        // Inisialisasi grafik kedua
+        Highcharts.chart('chart2', data2);
+    </script>
+</body>
+</html>
